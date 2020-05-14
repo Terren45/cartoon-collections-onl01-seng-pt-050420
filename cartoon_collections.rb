60ccad51acffbc22e530a785e10eccde0cd172c6
Terren Culberson <culbersonterren@yahoo.com>
2020-05-14 05:34:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,4 +15,10 @@ def long_planeteer_calls(planteteer_calls)
   
 end
 
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
 
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end

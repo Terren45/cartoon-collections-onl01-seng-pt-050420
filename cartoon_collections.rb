eaa67f8885f7e74b1a452cb61131a05b72c1f7aa
Terren Culberson <culbersonterren@yahoo.com>
2020-05-14 05:27:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(planteteer_calls)
   
 end
 
-def find_the_cheese(cheese)
+def find_the_cheese(cheese_types)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
